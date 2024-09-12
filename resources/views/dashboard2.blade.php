97e5ee82a1eebc320a0194be4b6f68605d4412df
yusupfey <yusup.ferdiansyah08@gmail.com>
2024-09-12 20:56:56 +0700

    recheck

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -76,68 +76,69 @@
             } */
         </style>
 @endpush
-@section('js')
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-     <script>
-        let lat = '';
-        let long = '';
-        let marker = null;
+    @section('js')
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+        <script>
+            let lat = '';
+            let long = '';
+            let marker = null;
 
-        map()
-        function getLocation() {
-            console.log('jalan');
+            map()
+            function getLocation() {
+                console.log('jalan');
             
-        if (navigator.geolocation) {
-            console.log(navigator.geolocation);
-         }
-        function showPosition(position) {
-            console.log(position);
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
-            // map(position.coords.latitude,position.coords.longitude)
-        }
-        
-        function map(){
-            var popup = L.popup();
-            var map = L.map('map');
+                if (navigator.geolocation) {
+                    console.log(navigator.geolocation);
+                }
+            }
+            function showPosition(position) {
+                console.log(position);
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                // map(position.coords.latitude,position.coords.longitude)
+            }
             
+            function map(){
+                var popup = L.popup();
+                var map = L.map('map');
                 
-                setTimeout(() => {
-                    setInterval(() => {
-                        getLocation()
-                            map.setView([lat,long], 16);
-                        // var marker = L.marker([51.5, -0.09]).addTo(map);
-            
-                        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            maxZoom: 19,
-                            // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                        }).addTo(map)
-                        let iconImg = L.icon({
-                            iconUrl: 'https://cdn3.iconfinder.com/data/icons/map-14/144/Map-10-512.png',
-                            iconSize:     [30, 30], // size of the icon
-                            shadowSize:   [10, 64], // size of the shadow
-                            // iconAnchor:   [50, 50], // point of the icon which will correspond to marker's location
-                            shadowAnchor: [4, 62],  // the same for the shadow
-                            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-                        })
-
-                        if (marker !== null) {
-                            map.removeLayer(marker)
-                        }
-                        marker = L.marker([lat,long],{icon:iconImg}).addTo(map).bindPopup("Your location.");
-                    }, 5000);
-        
-                    L.marker([-6.410176262551054, 106.96085579133336]).addTo(map).bindPopup('RSIA Kenari Graha Medika');
                     
-                    L.circle([-6.410176262551054, 106.96085579133336], {
-                        color: 'red',
-                        fillColor: '#f03',
-                        fillOpacity: 0.5,
-                        radius: 50
-                    }).addTo(map).bindPopup("RSIA Kenari Graha Medika Area.")
-    
-                }, 300);
-        }
+                    setTimeout(() => {
+                        setInterval(() => {
+                            getLocation()
+                                map.setView([lat,long], 16);
+                            // var marker = L.marker([51.5, -0.09]).addTo(map);
+                
+                            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                maxZoom: 19,
+                                // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                            }).addTo(map)
+                            let iconImg = L.icon({
+                                iconUrl: 'https://cdn3.iconfinder.com/data/icons/map-14/144/Map-10-512.png',
+                                iconSize:     [30, 30], // size of the icon
+                                shadowSize:   [10, 64], // size of the shadow
+                                // iconAnchor:   [50, 50], // point of the icon which will correspond to marker's location
+                                shadowAnchor: [4, 62],  // the same for the shadow
+                                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+                            })
+
+                            if (marker !== null) {
+                                map.removeLayer(marker)
+                            }
+                            marker = L.marker([lat,long],{icon:iconImg}).addTo(map).bindPopup("Your location.");
+                        }, 5000);
+            
+                        L.marker([-6.410176262551054, 106.96085579133336]).addTo(map).bindPopup('RSIA Kenari Graha Medika');
+                        
+                        L.circle([-6.410176262551054, 106.96085579133336], {
+                            color: 'red',
+                            fillColor: '#f03',
+                            fillOpacity: 0.5,
+                            radius: 50
+                        }).addTo(map).bindPopup("RSIA Kenari Graha Medika Area.")
+        
+                    }, 300);
+            }
      </script>
     @endsection
 </x-main-layout>
